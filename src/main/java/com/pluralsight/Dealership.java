77abba259197e8570b99e21f9ac891ae8bcc6c4f
Sahara <sahara.tm65@gmail.com>
2023-11-01 13:09:15 -0400

    Initial Commit

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+import java.util.ArrayList;
+public class Dealership {
+
+    String name;
+    String address;
+    String phone;
+    ArrayList<Vehicle> = new ArrayList<> ();
+
+
+    //Constructor
+    public Dealership(name, address, phone)
+    {
+      this.name = name;
+      this.address = address;
+      this.phone = phone;
+    }
+
+    public ArrayList <Vehicle> getVehiclesByprice()
+
+
+}
