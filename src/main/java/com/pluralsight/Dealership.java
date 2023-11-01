d8ac027c2c92ab08bc500a2cea3a938ef6510dc8
Sahara <sahara.tm65@gmail.com>
2023-11-01 13:44:34 -0400

    Finished CarDealership class

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,22 +1,80 @@
 package com.pluralsight;
+
 import java.util.ArrayList;
+import java.util.List;
+
 public class Dealership {
 
-    String name;
-    String address;
-    String phone;
-    ArrayList<Vehicle> = new ArrayList<> ();
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory = new ArrayList<>();
 
-
-    //Constructor
-    public Dealership(name, address, phone)
-    {
-      this.name = name;
-      this.address = address;
-      this.phone = phone;
+    public String getName() {
+        return name;
     }
 
-    public ArrayList <Vehicle> getVehiclesByprice()
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getAddress() {
+        return address;
+    }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    //Constructor
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public List<Vehicle> getVehiclesByprice(double max, double min) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByYear(String min, String max) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByColor(String color) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMileage(int min, int max) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByType(String vehicleType) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getAllVehicles() {
+
+        return null;
+    }
+
+    public void addVehicle() {
+
+    }
+
+    public void removeVehicle(Vehicle b) {
+
+    }
 }
