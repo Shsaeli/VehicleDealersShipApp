5bcb800b4d2ec5de4ff569abb5670a929a1e9beb
Sahara <sahara.tm65@gmail.com>
2023-11-01 13:30:53 -0400

    Vehicle Class Finished

---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,45 @@
+package com.pluralsight;
+
+public class Vehicle {
+
+    int vin;
+    int year;
+    String make;
+    String model;
+    String vehicleType;
+    String color;
+    int odometer;
+    double price;
+
+    public int getVin() {
+        return vin;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
