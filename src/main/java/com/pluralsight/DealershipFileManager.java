2c2af29757f81bc234ec4d0cf529631f13aedfde
Shankar Ale Magar <shankaralemagar91@gmail.com>
2023-11-02 09:28:03 -0400

    Added few things

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -24,7 +24,8 @@ public class DealershipFileManager {
                 Vehicle v = new Vehicle(Integer.parseInt(vehicleInfo[0]), Integer.parseInt(vehicleInfo[1]),vehicleInfo[2],vehicleInfo[3],vehicleInfo[4], vehicleInfo[5],Integer.parseInt(vehicleInfo[6]),Double.parseDouble(vehicleInfo[7]));
                 ListOfVehicle.add(v);
             }
-            new Dealership(dealershipInfo[0],dealershipInfo[1],dealershipInfo[2], ListOfVehicle);
+            d = new Dealership(dealershipInfo[0],dealershipInfo[1],dealershipInfo[2], ListOfVehicle);
+
 
         } catch (Exception e) {
             e.printStackTrace();
